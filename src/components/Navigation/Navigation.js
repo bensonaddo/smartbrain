9bcc03a80f4081d8c5eb2b84d0acfb8b937ee80e
baddo1 <benson.addo@gmfinancial.com>
2022-04-10 14:52:00 -0500

    added navigation component to the app

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+// Define Navigation Component
+const Navigation = () => {
+    return(
+        <nav style={{display: "flex", justifyContent: "flex-end"}}>
+            <p className="f3 link dim black underline pa3 pointer">Sign out</p>
+        </nav>
+    );
+}
+
+export default Navigation;
