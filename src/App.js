62f1821ba954d8cb2bf2969da82a982fcecc81d3
baddo1 <benson.addo@gmfinancial.com>
2022-04-10 16:40:43 -0500

    applying ImageLinkForm components to be rendered on the page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logos/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Logo />
+      <ImageLinkForm />
     </div>
   );
 }
