1d01b3b9f03f653752c3671764bf53b288bbdf0a
baddo1 <benson.addo@gmfinancial.com>
2022-04-10 15:36:05 -0500

    added changes to render the new logo component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logos/Logo';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
+      <Logo />
     </div>
   );
 }
